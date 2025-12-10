436f2994c94c6225f1444504547f6c7476d71b19
Rodolfo Oliveira <47088607+dorfusouza@users.noreply.github.com>
2025-12-10 15:33:04 -0300

    alteração do icone

--- App.tsx
+++ App.tsx
@@ -26,7 +26,6 @@ export default function App() {
           component={PreviewScreen}
           options={{ title: "Visualização" }}
         />
-        {/* teste */}
       </Stack.Navigator>
     </NavigationContainer>
   );
